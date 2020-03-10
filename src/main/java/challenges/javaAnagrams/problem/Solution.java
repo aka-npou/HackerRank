5590989cd74f8ddae3f607abfad4e6e3f3782458
npou <npou@mail.ru>
2020-03-10 11:01:04 +0300

    solve problem java-anagrams

---
+++ src/main/java/challenges/javaAnagrams/problem/Solution.java
@@ -0,0 +1,31 @@
+package challenges.javaAnagrams.problem;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        char[] chars;
+        chars = a.toLowerCase().toCharArray();
+        Arrays.sort(chars);
+        String sortedA = new String(chars);
+
+        chars = b.toLowerCase().toCharArray();
+        Arrays.sort(chars);
+        String sortedB = new String(chars);
+
+        return sortedA.equals(sortedB);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
---
+++ src/test/java/challenges/javaAnagrams/problem/SolutionTest.java
@@ -0,0 +1,22 @@
+package challenges.javaAnagrams.problem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void isAnagram1() {
+        boolean actual = Solution.isAnagram("anagramm", "marganaa");
+        boolean expected = false;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void isAnagram2() {
+        boolean actual = Solution.isAnagram("Hello", "hello");
+        boolean expected = true;
+        assertEquals(actual, expected);
+    }
+}
